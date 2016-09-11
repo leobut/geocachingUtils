f8969f4eb0cf51b2e1b9222ca4efd58081e61e9f
Leonard Butz <lbutz@controlthebit.ch>
2016-09-11 21:35:57 +0200

    extract common functions into common.js

---
+++ source/js/common.js
@@ -0,0 +1,16 @@
+var coordinateElement;
+var currentPageIsGeocacheDetailPage = true;
+
+(function findCoordinatesInPage(){
+	var selectorForEditedCoordinates = "a.edit-cache-coordinates > strong > span";
+	var selectorForUntouchedCoordinates = "#uxLatLon";
+
+	coordinateElement = $(selectorForUntouchedCoordinates);
+	if(coordinateElement.length === 0){
+		coordinateElement = $(selectorForEditedCoordinates);
+		if(coordinateElement.length === 0){
+			coordinateElement = undefined;
+			currentPageIsGeocacheDetailPage = false;
+		}
+	}
+})();
--- source/js/elevation.js
+++ source/js/elevation.js
@@ -1,9 +1,7 @@
 // IMPORTANT: You need to enter your own Google API key here for now:
 var apiKey = "";
-var coordinateElement = findCoordinatesInPage();
 
-
-if(coordinateElement !== undefined){
+if(currentPageIsGeocacheDetailPage){
 	var currentText = coordinateElement.text();
 	var latLon = getDdFromDms(currentText);
 	var elevationServiceUrl = "https://maps.googleapis.com/maps/api/elevation/json?locations="+latLon+"&key=" + apiKey;
@@ -12,44 +10,29 @@ if(coordinateElement !== undefined){
 		var elevationInMeters = Math.round(data.results[0].elevation * 100) / 100;
 		coordinateElement.text(currentText + " (" + elevationInMeters + "m)")
 	});
-}
 
-function findCoordinatesInPage(){
-	var selectorForEditedCoordinates = "a.edit-cache-coordinates > strong > span";
-	var selectorForUntouchedCoordinates = "#uxLatLon";
+	function getDdFromDms(input){
+		// input must be the same as on geocaching.com
+		// example: N 47° 30.159 E 008° 43.187
+		var convert = function(direction, degrees, minutes, seconds){
+			degrees = parseFloat(degrees);
+			minutes = parseFloat(minutes);
+			seconds = parseFloat(seconds); // seconds are already in decimal
 
-	var coordinateElement = $(selectorForUntouchedCoordinates);
-	if(coordinateElement.length === 0){
-		coordinateElement = $(selectorForEditedCoordinates);
-		if(coordinateElement.length === 0){
-			coordinateElement = undefined;
-		}
+
+			var dd = degrees + minutes/60 + seconds/60;
+
+		    if (direction == "S" || direction == "W") {
+		        dd = dd * -1;
+		    } // Don't do anything for N or E
+		    return dd;
+		};
+
+		var parts = input.split(/[.\s°]+/);
+	    var lat = convert(parts[0], parts[1], parts[2], "0."+parts[3]);
+	    var long = convert(parts[4], parts[5], parts[6], "0."+parts[7]);
+
+	    var result = lat + "," + long;
+		return result;
 	}
-
-	return coordinateElement;
-}
-
-function getDdFromDms(input){
-	// input must be the same as on geocaching.com
-	// example: N 47° 30.159 E 008° 43.187
-	var convert = function(direction, degrees, minutes, seconds){
-		degrees = parseFloat(degrees);
-		minutes = parseFloat(minutes);
-		seconds = parseFloat(seconds); // seconds are already in decimal
-
-
-		var dd = degrees + minutes/60 + seconds/60;
-
-	    if (direction == "S" || direction == "W") {
-	        dd = dd * -1;
-	    } // Don't do anything for N or E
-	    return dd;
-	};
-
-	var parts = input.split(/[.\s°]+/);
-    var lat = convert(parts[0], parts[1], parts[2], "0."+parts[3]);
-    var long = convert(parts[4], parts[5], parts[6], "0."+parts[7]);
-
-    var result = lat + "," + long;
-	return result;
 }
