e2938d3718e9dfa4c585ee271aa4bb0d0fa8b083
Leonard Butz <lbutz@controlthebit.ch>
2016-09-11 21:35:32 +0200

    Working friend list feature

--- source/js/friendFeatures.js
+++ source/js/friendFeatures.js
@@ -1,41 +1,51 @@
-var resultDisplay = $("div.CacheDetailNavigation");
-var friendLogList = $("<ul id='friendLogList'>");
-resultDisplay.append(friendLogList);
+if(currentPageIsGeocacheDetailPage){
 
-//TODO: This listener is not being called somehow...
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	console.log(message);
-	console.log(sender);
-	return false;
-});
+	var resultDisplay = $("div.CacheDetailNavigation");
+	var friendLogList = $("<ul id='friendLogList'>");
+	resultDisplay.append(friendLogList);
 
-injectCodeToReadUserToken();
+	injectCodeToReadUserToken();
+	fillFriendLogList(1);
 
 
-function fillFriendLogList(){
-	$.getJSON('/seek/geocache.logbook', {
-        tkn: friendLogList.data('userToken'),
-        idx: 1,
-        num: 100,
-        sp: false,
-        sf: true,
-        decrypt: false
-    }).done(function(response) {
-    	console.log(response);
-        if (response.status != 'success') {
-        	// TODO show error to user
-            console.log("error");
-            return;
-        };
+	function fillFriendLogList(page){
+		var userToken = friendLogList.attr('data-userToken');
+		$.getJSON('/seek/geocache.logbook', {
+	        tkn: userToken,
+	        idx: page,
+	        num: 100,
+	        sp: false,
+	        sf: true,
+	        decrypt: false
+	    }).done(function(response) {
+	    	if (response.status != 'success') {
+	        	friendLogList.text("There has been an error while loading friend logs.");
+	            return;
+	        } else {
+	        	if(response.pageInfo.idx < response.pageInfo.totalPages){
+	        		// load more friends if there are pages left
+	        		fillFriendLogList(++page);
+	        	} else {
+			        $.each(response.data, function (index, value){
+			        	var newFriendLogEntry = $("<li>");
+			        	var userImage = $("<img class='friendAvatar' src='https://img.geocaching.com/avatar/"+value.AvatarImage+"'>");
+			        	var logDetailTable = $("<table><tr><td><img src='/images/logtypes/"+value.LogTypeImage+"'> "+value.Visited+"</td></tr><tr><td>"+value.UserName+"</td></tr></table>");
 
-    });
-}
+			        	newFriendLogEntry.append(userImage).append(logDetailTable);
+			        	friendLogList.append(newFriendLogEntry);
+			        });
+			    }
+		    }
+	    });
+	}
 
-/*
-* Insert a piece of code that sends the userToken to the extension.
-*/
-function injectCodeToReadUserToken(){
-	var myId = chrome.runtime.id;
-	var scriptElement = $("<script>chrome.runtime.sendMessage('"+myId+"',{userToken: userToken});</script>");
-	$("html").append(scriptElement);
+	/*
+	* Insert a piece of code into the page that adds the userToken to the DOM of the friend list.
+	* If it is not in the DOM, but e.g. in data(), the exchange of data das not work --> it must be in the DOM.
+	* userToken is needed to load the friend logs and is not accessible otherwise within this script.
+	*/
+	function injectCodeToReadUserToken(){
+		var scriptElement = $("<script>$('#friendLogList').attr('data-userToken', userToken);</script>");
+		$("html").append(scriptElement);
+	}
 }
