95f4b75d09c7575385fe9fd520b579d4ad6e46c7
Lenny Butz <support@controlthebit.ch>
2016-10-08 19:10:11 +0200

    Move cssLint configuration to seperate file

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,42 +43,7 @@ module.exports = function(grunt) {
     csslint: {
       strict: {
         options: {
-          "adjoining-classes": false,
-          "ids": false,
-          "box-model": false,
-          "important": 2,
-          "known-properties": 2,
-          "box-sizing": 2,
-          "overqualified-elements": 2,
-          "display-property-grouping": 2,
-          "bulletproof-font-face": 2,
-          "compatible-vendor-prefixes": 2,
-          "regex-selectors": 2,
-          "errors": 2,
-          "duplicate-background-images": 2,
-          "duplicate-properties": 2,
-          "empty-rules": 2,
-          "selector-max-approaching": 2,
-          "gradients": 2,
-          "fallback-colors": 2,
-          "font-sizes": 2,
-          "font-faces": 2,
-          "floats": 2,
-          "star-property-hack": 2,
-          "outline-none": 2,
-          "import": 2,
-          "order-alphabetical": 2,
-          "underscore-property-hack": 2,
-          "rules-count": 2,
-          "qualified-headings": 2,
-          "selector-max": 2,
-          "shorthand": 2,
-          "text-indent": 2,
-          "unique-headings": 2,
-          "universal-selector": 2,
-          "unqualified-attributes": 2,
-          "vendor-prefix": 2,
-          "zero-units": 2
+          csslintrc: '.csslintrc'
         },
         src: ['build/**/*.css']
       }
