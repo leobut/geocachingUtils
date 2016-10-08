4e553c1fbd60c63aad41d04629b879df4441b341
Lenny Butz <support@controlthebit.ch>
2016-10-08 19:33:22 +0200

    Replace jsHint with esLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     manifest: grunt.file.readJSON('source/manifest.json'),
@@ -23,22 +25,11 @@ module.exports = function(grunt) {
         dest: 'build',
       }
     },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        esnext: true,
-        bitwise: true,
-        globals: {
-          jQuery: true
+    eslint: {
+        options: {
+            configFile: '.eslintrc'
         },
-        ignores: [
-          'build/js/lib/**/*.js'
-        ]
-      },
-      all: ['build/**/*.js']
+        target: ['build/**/*.js']
     },
     csslint: {
       strict: {
@@ -77,7 +68,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
@@ -85,6 +75,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-csslint');
 
   grunt.registerTask('cleanUp', ['clean'])
-  grunt.registerTask('build', ['cleanUp', 'copy', 'jshint', 'csslint']);
+  grunt.registerTask('build', ['cleanUp', 'copy', 'eslint', 'csslint']);
   grunt.registerTask('default', ['build', 'uglify', 'cssmin', 'compress']);
 };
