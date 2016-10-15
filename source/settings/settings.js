c9b6db687cba1c72a913ad9d4de695a34603cbfb
Lenny Butz <support@controlthebit.ch>
2016-10-15 18:16:56 +0200

    Translated settings page

--- source/settings/settings.js
+++ source/settings/settings.js
@@ -1,5 +1,5 @@
 
-function save_options() {
+function saveOptions(){
     var elevationMeasurement = document.getElementById('elevation_measurement').value,
       automaticallyDecryptHints = (document.getElementById('automatically_decrypt_hints').checked) ? 'true' : 'false',
       logEditorCountWords = (document.getElementById('log_editor_count_words').checked) ? 'true' : 'false',
@@ -15,7 +15,7 @@ function save_options() {
     logEditorShowInsertionPopup: logEditorShowInsertionPopup,
     showElevation: showElevation,
     showFriendLogs: showFriendLogs
-  }, function() {
+  }, function(){
     var status = document.getElementById('statusDisplay');
     status.textContent = chrome.i18n.getMessage('settings_settings_saved');
     setTimeout(function() {
@@ -24,7 +24,7 @@ function save_options() {
   });
 }
 
-function restore_options() {
+function restoreOptions(){
   chrome.storage.sync.get({
     // concrete values in here define the default if nothing was set yet by the user
     elevationMeasurement: 'meters',
@@ -33,7 +33,7 @@ function restore_options() {
     logEditorShowInsertionPopup: 'true',
     showElevation: 'true',
     showFriendLogs: 'true'
-  }, function(items) {
+  }, function(items){
     document.getElementById('elevation_measurement').value = items.elevationMeasurement;
     if(items.automaticallyDecryptHints === 'true'){ document.getElementById('automatically_decrypt_hints').checked = true; }
     if(items.logEditorCountWords === 'true'){ document.getElementById('log_editor_count_words').checked = true; }
@@ -43,5 +43,27 @@ function restore_options() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('saveButton').addEventListener('click', save_options);
+function loadLabels(){
+  function loadTextAndInsertInElementByKey(key){
+    document.getElementById(key).innerHTML = chrome.i18n.getMessage(key);
+  }
+
+  loadTextAndInsertInElementByKey('settings_main_title');
+  loadTextAndInsertInElementByKey('settings_detail_page_title');
+  loadTextAndInsertInElementByKey('settings_detail_page_show_friend_logs');
+  loadTextAndInsertInElementByKey('settings_detail_page_decrypt_hints');
+  loadTextAndInsertInElementByKey('settings_detail_page_show_elevation');
+  loadTextAndInsertInElementByKey('settings_elevation_meters');
+  loadTextAndInsertInElementByKey('settings_elevation_feet');
+  loadTextAndInsertInElementByKey('settings_log_editor_main_title');
+  loadTextAndInsertInElementByKey('settings_log_editor_count_words');
+  loadTextAndInsertInElementByKey('settings_log_editor_count_words_note');
+  loadTextAndInsertInElementByKey('settings_log_editor_show_insertion_popup');
+  loadTextAndInsertInElementByKey('settings_footnote');
+
+  document.getElementById('saveButton').innerText = chrome.i18n.getMessage('settings_save_button');
+}
+
+document.addEventListener('DOMContentLoaded', loadLabels);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('saveButton').addEventListener('click', saveOptions);
